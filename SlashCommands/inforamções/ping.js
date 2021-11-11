0f71c2c9909eb0283ce3087a1393373c8935d4d0
Lua <84294591+PontinhoAleatorio@users.noreply.github.com>
2021-11-10 22:10:04 -0300

    Create ping.js

---
+++ SlashCommands/inforamções/ping.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const c = require("../../infs")
+module.exports = {
+    name: "ping",
+    description: "🏓 | Ping Pong",
+    type: 'CHAT_INPUT',
+
+
+    run: async (client, interaction, args) => {
+      
+  const embed = new Discord.MessageEmbed()
+  .setTitle("🏓 | Pong! Estou ativa! UwU")
+  .setColor("#b8fffa") 
+  .setThumbnail(c.thumb)
+  .addField("Latência da API:", `${Math.round(client.ws.ping)}ms`, true)
+
+
+      interaction.followUp({embeds: [embed]})
+    }}
