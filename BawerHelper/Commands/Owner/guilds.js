abe7dda7f2ee0f0c10f36965050b0913ccefa80a
Lua <84294591+PontinhoAleatorio@users.noreply.github.com>
2021-11-10 22:45:09 -0300

    Servidores onde seu bot esta

---
+++ BawerHelper/Commands/Owner/guilds.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+     exports.run = async (client, message, args) => {
+        let clientGuilds = message.client.guilds.cache;
+        let messageObj = Discord.Util.splitMessage(
+            clientGuilds.map(g => '\`' + g.id + `\` **|** \`` + g.name + `\` **|** \`` + g.members.cache.size + '\` ') || 'None'
+        );
+        if (messageObj.length == 1) {
+            message.channel.send(messageObj[0]);
+        } else {
+            for (i = 0; messageObj.length < i; i++) {
+                message.channel.send(messageObj[i]);
+            }
+        }
+    }
+module.exports.help = {
+	aliases: ["servidores", "svr"],
+	name: "guilds"
+}
