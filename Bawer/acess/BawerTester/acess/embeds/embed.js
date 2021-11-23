c6c2ee8bd56b0a8cdc33b9e4e9214dedcbfe6576
Lua <84294591+PontinhoAleatorio@users.noreply.github.com>
2021-11-23 19:41:12 -0300

    Create embed.js

---
+++ Bawer/acess/BawerTester/acess/embeds/embed.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    color: "#08ddf5",
+    color2: '#f2da80',
+    thumb:"https://cdn.discordapp.com/avatars/867825474746843136/d9f5c6c3cd5db9b0dd42139d0bf6368f.webp?size=4096",
+    thumb2:"https://cdn.discordapp.com/attachments/870785519158710312/900554244300623892/6_Sem_Titulo_20211020222030.png",
+    thumb3:"https://cdn.discordapp.com/attachments/871443795961581598/908428604302573628/1636656637609.png",
+    thumb4:"https://cdn.discordapp.com/attachments/858065911062855680/908736499120496720/1636729873666.png"
+    }
+   
